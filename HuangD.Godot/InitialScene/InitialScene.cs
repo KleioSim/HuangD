ecdf526f964aace6264da41cd5cd5f903f57be0e
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-07-05 17:44:13 +0800

    init

---
+++ HuangD.Godot/InitialScene/InitialScene.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+using System;
+
+public partial class InitialScene : Control
+{
+    public void Start()
+    {
+        GetTree().ChangeSceneToFile("res://MainScene/MainScene.tscn");
+    }
+
+    public void Load()
+    {
+        var mapScene = ResourceLoader.Load<PackedScene>("res://MapScene/MapScene.tscn").Instantiate();
+        GetTree().Root.AddChild(mapScene);
+    }
+}
