e040ceb8eccef6f0b1e75bbd3795ac6a8b73c7f4
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-08-01 11:04:58 +0800

    fixed  isuess

--- HuangD.Godot/MapScene/MapScene.cs
+++ HuangD.Godot/MapScene/MapScene.cs
@@ -11,27 +11,29 @@ public partial class MapScene : Node2D
     PopCountMap PopCountMap => GetNode<PopCountMap>("CanvasLayer/PopCountMap");
     TerrainMap TerrainMap => GetNode<TerrainMap>("CanvasLayer/TerrainMap");
     MapCamera2D Camera => GetNode<MapCamera2D>("CanvasLayer/Camera2D");
-    InstancePlaceholder PoliticalInfo => GetNode<InstancePlaceholder>("CanvasLayer/PoliticalInfo");
+    InstancePlaceholder PoliticalInfoPlaceHolder => GetNode<InstancePlaceholder>("CanvasLayer/PoliticalInfo");
 
     public override void _Ready()
     {
         var session = this.GetSession();
 
-        foreach(var cell in session.MapCells.Values)
+        foreach (var cell in session.MapCells.Values)
         {
             TerrainMap.AddOrUpdate(cell.Index, cell.TerrainType);
-            if(cell.TerrainType != TerrainType.Water) 
+            if (cell.TerrainType != TerrainType.Water)
             {
                 PopCountMap.AddOrUpdate(cell.Index, cell.PopCount);
                 ProvinceMap.AddOrUpdate(cell.Index, cell.ProvinceId);
             }
         }
 
-        foreach(var province in session.Provinces.Values)
+        foreach (var province in session.Provinces.Values)
         {
-            var politicalInfo = PoliticalInfo.CreateInstance() as PoliticalInfo;
-            politicalInfo.Position = ProvinceMap.GetPawnLocation(province.Key) ;
+            var politicalInfo = PoliticalInfoPlaceHolder.CreateInstance() as PoliticalInfo;
+            politicalInfo.Position = ProvinceMap.GetPawnLocation(province.Key);
             politicalInfo.OnZoomed(Camera.Zoom);
+
+            Camera.Connect(MapCamera2D.SignalName.OnZoomed, new Callable(politicalInfo, PoliticalInfo.MethodName.OnZoomed));
         }
 
         var terrainMap = GetNode<TerrainMap>("CanvasLayer/TerrainMap");
