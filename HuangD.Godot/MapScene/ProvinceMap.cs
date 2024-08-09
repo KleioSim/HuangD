26b618b4070663d95036caa88e150aecd6a8e1b9
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-08-09 22:46:30 +0800

    change province position

--- HuangD.Godot/MapScene/ProvinceMap.cs
+++ HuangD.Godot/MapScene/ProvinceMap.cs
@@ -7,6 +7,8 @@ using Index = HuangD.Sessions.Maps.Index;
 
 public partial class ProvinceMap : TileMap
 {
+    private TileMap PoliticalMap => GetNode<TileMap>("PoliticalMap");
+
     private Random random = new Random();
     private Dictionary<string, int> dict = new Dictionary<string, int>();
     private List<Color> colors = new List<Color>();
@@ -47,7 +49,6 @@ public partial class ProvinceMap : TileMap
     {
         var cells = this.GetUsedCells(dict[key]);
 
-
         var edges = cells.Where(c =>
         {
             var neighbors = MapCell.IndexMethods.GetNeighborCells(new Index(c.X, c.Y)).Values
@@ -55,19 +56,54 @@ public partial class ProvinceMap : TileMap
             return neighbors.Any(n => GetCellSourceId(dict[key], n) == -1);
         });
 
-        var centerCell = cells.First();
-        var distance = int.MinValue;
-        foreach (var cell in cells)
+        var cellsWithDistance = cells.Select(c =>
         {
-            var minDist = (int)edges.Select(e => (e - cell).Length()).Min();
-            if (minDist > distance)
-            {
-                distance = minDist;
-                centerCell = cell;
-            }
+            var distances = edges.Select(e => (e - c).Length());
+            return (cell: c, minDistance: distances.Min(), maxDistance: distances.Max());
+        });
+
+        var centerCellWithDistance = cellsWithDistance.GroupBy(x => x.minDistance)
+            .MaxBy(x => x.Key)
+            .MaxBy(x => x.maxDistance);
+
+        if (centerCellWithDistance.minDistance > 1)
+        {
+            return this.MapToLocal(centerCellWithDistance.cell);
         }
 
-        return MapToLocal(centerCell);
+        var expendCells = cells.SelectMany(c =>
+        {
+            var baseCells = new Vector2I[]
+            {
+                new Vector2I(0,0),
+                new Vector2I(0,1),
+                new Vector2I(1,0),
+                new Vector2I(1,1)
+            };
+
+            return baseCells.Select(x => c * 2 + x);
+        }).ToArray();
+
+        edges = expendCells.Where(c =>
+        {
+            var neighbors = MapCell.IndexMethods.GetNeighborCells(new Index(c.X, c.Y)).Values
+                .Select(n => new Vector2I(n.X, n.Y));
+            return neighbors.Any(n => !expendCells.Contains(n));
+        });
+
+        cellsWithDistance = expendCells.Select(c =>
+        {
+            var distances = edges.Select(e => (e - c).Length());
+            return (cell: c, minDistance: distances.Min(), maxDistance: distances.Max());
+        });
+
+        var centerCell = cellsWithDistance.GroupBy(x => x.minDistance)
+            .MaxBy(x => x.Key)
+            .MaxBy(x => x.maxDistance)
+            .cell;
+
+        return PoliticalMap.MapToLocal(centerCell);
+
     }
 
     internal string LocalToProvince(Vector2 vector2)
