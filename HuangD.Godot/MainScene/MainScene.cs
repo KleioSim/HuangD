88acec4ac98e86c98c5c846930319b39438d4047
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-08-11 17:53:39 +0800

    Add MainScene

--- HuangD.Godot/MainScene/MainScene.cs
+++ HuangD.Godot/MainScene/MainScene.cs
@@ -1,7 +1,16 @@
+using Chrona.Engine.Godot;
 using Godot;
 using System;
 
-public partial class MainScene : Control
+public partial class MainScene : ViewControl
 {
+    protected override void Initialize()
+    {
 
+    }
+
+    protected override void Update()
+    {
+
+    }
 }
