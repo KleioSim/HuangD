8fea1ba664a4d285234a9aa2917b9d24e4536710
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-10-21 22:47:07 +0800

    ShowTotalArmyCount

--- HuangD.Godot/MainScene/DetailPanels/PlayerArmyDetailPanels/PlayerArmyDetailPanel.cs
+++ HuangD.Godot/MainScene/DetailPanels/PlayerArmyDetailPanels/PlayerArmyDetailPanel.cs
@@ -1,8 +1,25 @@
 using Chrona.Engine.Godot;
 using Godot;
+using HuangD.Godot.Utilties;
 using System;
+using System.Linq;
 
 public partial class PlayerArmyDetailPanel : PanelContainer, IView
 {
+    public Label TotalArmyCount => GetNode<Label>("VBoxContainer/Title/HBoxContainer/Value");
 
+    public override void _Process(double delta)
+    {
+        var view = this as IView;
+        if (!view.IsDirty()) { return; }
+
+        var playerArmyData = this.GetSelectEntity().Current as PlayerArmyData;
+        if(playerArmyData == null) 
+        {
+            QueueFree();
+            return;
+        }
+
+        TotalArmyCount.Text = playerArmyData.localArmies.Sum(x=>x.Count).ToString();
+    }
 }
