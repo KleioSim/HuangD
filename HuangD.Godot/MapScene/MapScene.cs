c4ad387567f045f6aed14edbd4b5092a0b8b652a
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-11-08 14:30:27 +0800

    Move Map to center

--- HuangD.Godot/MapScene/MapScene.cs
+++ HuangD.Godot/MapScene/MapScene.cs
@@ -1,23 +1,15 @@
-﻿using DynamicData;
-using Godot;
-using HuangD.Godot.Utilties;
-using HuangD.Sessions;
-using HuangD.Sessions.Maps;
-using HuangD.Sessions.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Linq;
+﻿using Godot;
 
 public partial class MapScene : GraphEdit
 {
     GraphElement GraphElement => GetNode<GraphElement>("GraphElement");
     BaseMap BaseMap => GetNode<BaseMap>("GraphElement/BaseMap");
-    PoliticalContainer PoliticalContainer => GetNode<PoliticalContainer>("GraphElement/MarginContainer/PoliticalContainer");
+    PoliticalContainer PoliticalContainer => GetNode<PoliticalContainer>("GraphElement/BaseMap/PoliticalContainer");
 
     public override void _Ready()
     {
         GraphElement.Size = BaseMap.GetSize();
+        GraphElement.PositionOffset = this.GetViewportRect().Size / 2 - GraphElement.Size / 2;
 
         PoliticalContainer.BuildPoliticalInfos(BaseMap.GetProvinceCenter);
     }
