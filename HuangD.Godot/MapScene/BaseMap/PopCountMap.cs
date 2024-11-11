9b3b45449fecb3498da9e07ccb2f49b611d41444
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-11-11 10:38:49 +0800

    fixed isuess

--- HuangD.Godot/MapScene/BaseMap/PopCountMap.cs
+++ HuangD.Godot/MapScene/BaseMap/PopCountMap.cs
@@ -1,23 +1,33 @@
 ﻿using Godot;
+using HuangD.Sessions;
 using HuangD.Sessions.Maps;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public partial class PopCountMap : TileMap
+public partial class PopCountMap : Node2D
 {
+    private TileMapLayer TileMapLayer => GetNode<TileMapLayer>("TileMapLayer");
+
     public override void _Ready()
     {
-        for (int i = 0; i <= 10; i++)
+        for (int i = 0; i < 10; i++)
         {
-            this.AddLayer(i);
-            this.SetLayerModulate(i, new Color(1f, (10 - i) * 0.1f, (10 - i) * 0.1f));
+            var newTileMapLayer = TileMapLayer.Duplicate() as TileMapLayer;
+            TileMapLayer.AddSibling(newTileMapLayer);
         }
     }
+
     internal void AddOrUpdate(IEnumerable<HuangD.Sessions.Maps.Index> indexes, int layerId)
     {
+        var layers = TileMapLayer.GetParent().GetChildren().OfType<TileMapLayer>().ToArray();
+
+        layers[layerId].Modulate = new Color(1f, (10 - layerId) * 0.1f, (10 - layerId) * 0.1f);
+
         foreach (var index in indexes)
         {
-            SetCell(layerId, new Vector2I(index.X, index.Y), 0, Vector2I.Zero, 0);
+            layers[layerId].SetCell(new Vector2I(index.X, index.Y), 0, Vector2I.Zero, 0);
         }
+
     }
 }
